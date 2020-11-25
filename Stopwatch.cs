95f96e457a0c69e449eaccb7246b4f2a7601b50c
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2020-11-25 15:08:10 +0200

    Add files via upload

---
+++ Stopwatch.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Stopwatch 
+{
+	class Program
+{
+    static void Main(string[] args)
+    {
+	// Create new stopwatch
+	Stopwatch stopwatch = new Stopwatch();
+
+	// Begin timing
+	stopwatch.Start();
+
+	// Do something
+	for (int i = 0; i < 1000; i++)
+	{
+	    Thread.Sleep(1);
+	}
+
+	// Stop timing
+	stopwatch.Stop();
+
+	// Write result
+	Console.WriteLine("Time elapsed: {0}",
+	    stopwatch.Elapsed);
+    }
+}
+}
